541c9160f7ac392f65e8b5f70cd62326738c2e04
Alababir <138342350+Alababir@users.noreply.github.com>
2024-06-17 16:52:27 -0300

    Update opencv.py
    colocando caixas em volta das cabeças

--- scripts/opencv.py
+++ scripts/opencv.py
@@ -5,5 +5,9 @@ img = cv2.imread("imagens/imagen1.jpg")
 imagen_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 rostos = carrega.detectMultiScale(imagen_cinza)
 print(rostos)
+
+for(x, y, l, a) in rostos:
+    cv2.rectangle(img, (x, y), (x + l, y + a), (0, 255, 0), 2)
+
 cv2.imshow("rostos", img)
 cv2.waitKey()
