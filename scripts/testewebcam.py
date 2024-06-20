609b431cbe112cf18bfdff0dc84e8c6eba66df8f
Alababir <alababir8@gmail.com>
2024-06-20 19:21:02 -0300

    colocando no novo not

--- scripts/testewebcam.py
+++ scripts/testewebcam.py
@@ -1,6 +1,15 @@
 import cv2
 
 webcam = cv2.VideoCapture(0)
+carrega = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+video_cinza = cv2.cvtColor(webcam, cv2.COLOR_BGR2GRAY)
+
+rostos = carrega.detectMultiScale(video_cinza)
+
+print(rostos)
+
+for(x, y, l, a) in rostos:
+    cv2.rectangle(webcam, (x, y), (x + l, y + l), (255, 0, 255), 1)
 
 while True:
     camera, frame = webcam.read()
